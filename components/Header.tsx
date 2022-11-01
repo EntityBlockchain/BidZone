f532ae9e2d14680b6aee448896d9005cb5353d30
brodyamcfarland <brodyamcfarland@gmail.com>
2022-11-01 11:12:54 -0700

    Finished Header, Added Products, Created Product Cards

--- components/Header.tsx
+++ components/Header.tsx
@@ -31,15 +31,13 @@ const Header = () => {
                             Connect Wallet
                         </button>
                     )}
-                    <p className="headerLinks">Deals</p>
-                    <p className="headerLinks">Contact</p>
                 </div>
                 <div className="flex items-center space-x-4 text-sm">
-                    <p className="headerLinks">Ship to</p>
+                    <p className="headerLinks">About</p>
                     <p className="headerLinks">Sell</p>
                     <p className="headerLinks">Watchlist</p>
 
-                    <Link className="link" href="/addItem">
+                    <Link className="link font-bold" href="/addItem">
                         List Item
                     </Link>
                     <AiOutlineBell className="h-6 w-6" />
@@ -48,7 +46,7 @@ const Header = () => {
             </nav>
             <hr className="mt-2" />
             <section className="flex items-center space-x-2 py-3">
-                <div className="h-16 w-16 sm:w-20 md:w-28 cursor-pointer flex-shrink-0">
+                <div className="flex relative h-16 w-16 sm:w-20 md:w-28 cursor-pointer flex-shrink-0">
                     <Link href="/">
                         <Image
                             className="h-full w-full object-contain"
@@ -58,6 +56,9 @@ const Header = () => {
                             height={100}
                         />
                     </Link>
+                    <p className="hidden md:inline absolute top-12 text-xs font-bold uppercase text-purple-400 tracking-[6px] pl-1">
+                        Auctions
+                    </p>
                 </div>
                 <button className="hidden lg:flex flex-row items-center w-20 space-x-2">
                     <p className="text-gray-300 text-sm">Categories</p>
@@ -73,15 +74,24 @@ const Header = () => {
                     />
                 </div>
 
-                <button className="hidden sm:inline px-4 md:px-8 py-2 border">
+                <button className="rounded-md hidden sm:inline px-4 md:px-8 py-2 bg-white/20 hover:bg-white/30 duration-500">
                     Search
                 </button>
 
                 <Link href="/create">
-                    <button className="hidden sm:inline px-4 md:px-8 py-2 border">
+                    <button className="rounded-md hidden sm:inline px-4 md:px-8 py-2 bg-purple-900/50 hover:bg-purple-900/60 duration-500">
                         List Item
                     </button>
                 </Link>
+            </section>
+            <hr />
+            <section className="flex py-3 space-x-6 text-xs md:text-sm whitespace-nowrap px-6 justify-center">
+                <p className="filterLinks">Home</p>
+                <p className="filterLinks">Tech</p>
+                <p className="filterLinks">Games</p>
+                <p className="filterLinks">NFT</p>
+                <p className="filterLinks">Music</p>
+                <p className="filterLinks">Other</p>
             </section>
         </div>
     );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,23 @@
 import Head from "next/head";
-import Image from "next/image";
 import Header from "../components/Header";
+import {
+    useActiveListings,
+    useContract,
+    MediaRenderer,
+} from "@thirdweb-dev/react";
+import PulseLoader from "react-spinners/PulseLoader";
+import { ListingType } from "@thirdweb-dev/sdk";
+import { BsCashCoin } from "react-icons/bs";
+import { RiAuctionLine } from "react-icons/ri";
 
 const Home = () => {
+    const { contract } = useContract(
+        process.env.NEXT_PUBLIC_MARKETPLACE_CONTRACT,
+        "marketplace"
+    );
+    const { data: listings, isLoading: loadingListings } =
+        useActiveListings(contract);
+    console.log(listings);
     return (
         <div className="bg-black min-h-screen text-white">
             <Head>
@@ -10,7 +25,71 @@ const Home = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Header />
-            <h1 className="text-red-500">Hello</h1>
+            <main className="max-w-6xl mx-auto p-2">
+                {loadingListings ? (
+                    <p className="text-center">
+                        <PulseLoader color="purple" size={12} />
+                    </p>
+                ) : (
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mx-auto">
+                        {listings?.map((listing) => (
+                            <div
+                                className="flex flex-col card hover:scale-105 transition-all duration-300 ease-out"
+                                key={listing.id}
+                            >
+                                <div className="flex flex-1 flex-col pb-2 items-center">
+                                    <MediaRenderer
+                                        className="w-44"
+                                        src={listing.asset.image}
+                                    />
+                                </div>
+                                <div className="pt-2 space-y-4">
+                                    <div>
+                                        <h2 className="text-md truncate">
+                                            {listing.asset.name}
+                                        </h2>
+                                        <hr />
+                                        <p className="truncate text-sm text-gray-500 mt-2">
+                                            {listing.asset.description}
+                                        </p>
+                                    </div>
+                                    <p>
+                                        <span className="font-bold mr-1">
+                                            {
+                                                listing
+                                                    .buyoutCurrencyValuePerToken
+                                                    .displayValue
+                                            }
+                                        </span>
+                                        {
+                                            listing.buyoutCurrencyValuePerToken
+                                                .symbol
+                                        }
+                                    </p>
+                                    <div
+                                        className={`flex items-center p-2 space-x-1 justify-end text-xs w-fit ml-auto rounded-lg cursor-pointer ${
+                                            listing.type === ListingType.Direct
+                                                ? "bg-gradient-to-br from-blue-600 to-gray-900"
+                                                : "bg-gradient-to-br from-orange-600 to-gray-900"
+                                        }`}
+                                    >
+                                        <p>
+                                            {listing.type === ListingType.Direct
+                                                ? "Buy Now"
+                                                : "Auction"}
+                                        </p>
+                                        {listing.type === ListingType.Direct ? (
+                                            <BsCashCoin className="h-4" />
+                                        ) : (
+                                            <RiAuctionLine className="h-4" />
+                                        )}
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </main>
         </div>
     );
 };
